96c861b628efa9112b480c77be2d8e651026d34a
Vinay Kumar <149612614+thevinaykumar@users.noreply.github.com>
2026-05-22 17:55:08 +0530

    Delete CA1.js

--- CA1.js
+++
@@ -1,8 +0,0 @@
-function showNoticePopup() {
-  alert("IMPORTANT: Voluntary recall notice on 2024 MULE PRO-FX™, PRO-FXR™ & PRO-FXT™ 1000 models.");
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  showNoticePopup();
-
-});
