b3964162bc857d5307da815508808677fe836d46
Vinay Kumar <149612614+thevinaykumar@users.noreply.github.com>
2026-05-22 17:57:45 +0530

    Add files via upload

---
+++ CA1.js
@@ -0,0 +1,8 @@
+function showNoticePopup() {
+  alert("IMPORTANT: Voluntary recall notice on 2024 MULE PRO-FX™, PRO-FXR™ & PRO-FXT™ 1000 models.");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  showNoticePopup();
+
+});
